cea3e778517f2ab2142585716a348bb5ea37b889
ahmedyar123 <ahmeda.abbasi99@gmail.com>
2020-07-30 21:41:33 +0500

    Maped data and added handleRemoveTour Function in Tourlist.js

--- src/Components/TourList/index.js
+++ src/Components/TourList/index.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 import "./TourList.scss";
 import Tour from "../Tour";
+import { tourData } from "../tourData";
 
 export default class TourList extends Component {
   state = {
-    tours: [],
+    tours: tourData,
+  };
+
+  handleRemoveTour = (id) => {
+    const { tours } = this.state;
+    const filteredTour = tours.filter((tour) => tour.id !== id);
+    this.setState({
+      tours: filteredTour,
+    });
   };
   render() {
+    const { tours } = this.state;
     return (
       <section className="tourlist">
-        <Tour />
+        {tours.map((tour) => {
+          return (
+            <Tour
+              key={tour.id}
+              tour={tour}
+              handleRemoveTour={this.handleRemoveTour}
+            />
+          );
+        })}
       </section>
     );
   }
